5ca090e593bb2408ca8bfa0afae5beb9787840a0
SenalAriyaratne <ariyaratne.senal@gmail.com>
2024-12-31 21:09:16 +0530

    minor updates

--- Endeavor.ReactUi/react-ui/src/App.js
+++ Endeavor.ReactUi/react-ui/src/App.js
@@ -4,36 +4,61 @@ import TitleBar from './components/TitleBar';
 import Toolbar from './components/ToolBar';
 import Dashboard from './pages/Dashboard';
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
-
+import CreateAccount from './pages/CreateAccount';
+import Projects from './pages/Projects';
+import Chat from './pages/Chat';
 function App() {
 
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [showCreateAccount, setShowCreateAccount] = useState(false);
+
+  
 
   const handleSignIn = () => {
     setIsSignedIn(true);
   };
 
+  const handleCreateAccount = () => {
+    setShowCreateAccount(true);
+  };
+
+  const handleCloseCreateAccount = () => {
+    setShowCreateAccount(false);
+  };
+
+
   return (
     <Router>
-    <div className="flex flex-col h-screen">
-      <TitleBar isSignedIn={isSignedIn} onSignIn={handleSignIn} />
+    <div className="flex flex-col h-screen overflow-auto">
+      <TitleBar isSignedIn={isSignedIn} onSignIn={handleSignIn} onCreateAccount={handleCreateAccount} />
       {isSignedIn ? (
-        <div className="flex flex-grow">
-          <Toolbar />
+        <div className="flex flex-grow ">
+          <Toolbar/>
           <div className="flex-grow">
             {/* Other components */}
+            
             <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/chat" element={<Chat />} /> 
             </Routes>
           </div>
         </div>
       ) : (
-        <Home />
+        showCreateAccount ? (
+          <CreateAccount  onClose={handleCloseCreateAccount}/>
+        ) : (
+         <Home />
+        )
       )}
     </div>
     </Router>
+   
+   
+    
   );
 }
 
