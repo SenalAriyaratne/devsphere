73c0e7e5a03373ca60a452ca852205e9ad06255e
SenalAriyaratne <ariyaratne.senal@gmail.com>
2024-12-31 21:05:32 +0530

    add new card to create and funcin dashboard

--- Endeavor.ReactUi/react-ui/src/components/AddTaskAndProjectCard.js
+++ Endeavor.ReactUi/react-ui/src/components/AddTaskAndProjectCard.js
@@ -4,14 +4,11 @@ const AddTaskAndProjectCard = () => {
     return (
         <div className="bg-white pb-2 rounded-lg shadow-lg w-full relative">
             <div className="grid grid-cols-4 grid-rows-1 ">
-                <div className="col-span-2 row-span-1 flex items-center  bg-blue-300  rounded-br-lg">
-                    <h2 className="text-2xl font-bold ml-1 text-blue-700">Quick</h2>
-                </div>
-                <div></div>
+             
                 <div className="col-span-2 row-span-1 flex items-center justify-center p-2">
                     <button className="bg-transparent text-blue-500 border-2 border-blue-500 rounded-full h-10 w-full flex items-center justify-center space-x-2 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white" style={{ fontFamily: 'sans-serif', fontWeight: 'bold' }}>
                         <span className="material-icons">add</span>
-                        <span>Create New Project</span>
+                        <span>New Project</span>
                     </button>
                 </div>
                 <div className="col-span-2 row-span-1 flex items-center justify-center p-2">
